f45eceeb93382469384700503d306699ae45b419
abdurrahman23ye <abdurrahman.kursicin@gmail.com>
2022-05-18 17:40:30 +0300

    LogoutUser

--- src/test/java/Test6_Contact_Us_Form.java
+++ src/test/java/Test6_Contact_Us_Form.java
@@ -3,9 +3,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.time.Duration;
 
 public class Test6_Contact_Us_Form {
@@ -38,7 +42,7 @@ public class Test6_Contact_Us_Form {
 
 
         @Test
-        public void test1() throws InterruptedException {
+        public void test1() throws InterruptedException, AWTException {
 
 
             Assert.assertTrue(driver.findElement(By.xpath("//html")).isDisplayed());
@@ -48,11 +52,43 @@ public class Test6_Contact_Us_Form {
             Assert.assertTrue(driver.findElement(By.xpath("(//h2[@class='title text-center'])[2]")).isDisplayed());
 
             driver.findElement(By.xpath("//input[@name='name']")).sendKeys("ali");
-            driver.findElement(By.xpath("//input[@name='email']")).sendKeys("ali@");
+            driver.findElement(By.xpath("//input[@name='email']")).sendKeys("ali@gmail.com");
             driver.findElement(By.xpath("//input[@name='subject']")).sendKeys("olaylar olaylar");
             driver.findElement(By.xpath("//textarea[@id='message']")).sendKeys("mesaj");
-            Thread.sleep(500);
-            driver.findElement(By.xpath("//input[@type='file']")).click();
+
+            JavascriptExecutor xy= (JavascriptExecutor) driver;
+
+            xy.executeScript("window.scrollBy(0,1000)");
+
+            WebElement file=driver.findElement(By.xpath("//input[@type='file']"));
+
+            file.
+                    sendKeys("C:\\Users\\asus\\IdeaProjects\\AutomationExerciseComTests\\src\\test\\java\\290.txt");
+
+            driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+            driver.switchTo().alert().accept();
+
+            Assert.assertTrue(driver.findElement(By.xpath("//div[@class='status alert alert-success']")).isDisplayed());
+            driver.findElement(By.xpath("//span")).click();
+
+            Assert.assertTrue(driver.findElement(By.xpath("//html")).isEnabled());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
